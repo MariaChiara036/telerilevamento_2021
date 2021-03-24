7920ef2cca29d453a1ed56f6ef5432bf8944e1af
MariaChiara036 <80686094+MariaChiara036@users.noreply.github.com>
2021-03-24 12:53:27 +0100

    Update R_code_remote_sensing_first.r

--- R_code_remote_sensing_first.r
+++ R_code_remote_sensing_first.r
@@ -3,6 +3,7 @@ setwd("C:/lab/") #windows
 library(raster)
 p224r63 <- brick("p224r63_1988_masked.grd")
 brick("p224r63_1988_masked.grd")
+#funz. brick importa l'immagine
 class      : RasterBrick 
 dimensions : 1499, 2967, 4447533, 7  (nrow, ncol, ncell, nlayers)
 resolution : 30, 30  (x, y)
@@ -21,6 +22,49 @@ plot(p224r63, col=cl)
 #colour change -> new
 cl<-colorRampPalette(c("black", "yellow", "blue", "aquamarine"))(100)
 plot(p224r63, col=cl)
+#day 3
+#b1 banda blu;b2 banda verde;b3 banda rosso;b4 infrarosso vicino;b5 infrarosso medio;b6 infrarosso termico;b7 infrarosso medio; sre=spectral reflectance
+#funzione dev.off() pulisce la finestra grafica
+plot(p224r63$B1_sre)
+#funzione per plottare solo la banda del blu
+cls <- colorRampPalette(c("red","pink","orange","purple")) (200)
+plot(p224r63$B1_sre, col=cls)
+plot(p224r63$B2_sre)
+#plottato la banda del verde
+par(mfrow=c(1,2))
+#tramite questa funzione posso visualizzare nel grafico due bande in particolare, possibile scegliere il tipo di visualizzazione in n.di righe e colonne
+plot(p224r63$B1_sre)
+plot(p224r63$B2_sre)
+#cosi visualizzo i due plot su una riga e due colonne
+par(fmrow=c(2,1))
+#visualizzazione su due righe e una colonna
+plot(p224r63$B1_sre)
+plot(p224r63$B2_sre)
+#visualizzazione grafico come da riga 40
+par(mfrow=c(4,1))
+plot(p224r63$B1_sre)
+plot(p224r63$B2_sre)
+plot(p224r63$B3_sre)
+plot(p224r63$B4_sre)
+par(mfrow=c(2,2))
+#visualizzazione quadrata
+ plot(p224r63$B4_sre)
+ plot(p224r63$B3_sre)
+ plot(p224r63$B2_sre)
+ plot(p224r63$B1_sre)
+clb <- colorRampPalette(c("dark blue","blue","light blue")) (100)
+plot(p224r63$B1_sre, col=clb)
+#cambio la palette solo per la banda del blu, specificando che l'argomento per il colore di questa banda è clb
+clg <- colorRampPalette(c("dark green","green","light green")) (100)
+plot(p224r63$B2_sre, col=clg)
+#cambio la palette solo per il verde, argomento specifico clg
+clr <- colorRampPalette(c("dark red","red","pink")) (100)
+ plot(p224r63$B3_sre, col=clr)
+#palette per il rosso, clr
+clnir <- colorRampPalette(c("red","orange","yellow")) (100)
+plot(p224r63$B4_sre, col=clnir)
+#palette per il vicino infrarosso individuata con clnir
+#col comando plot mi carica ogni volta ciò che io ho impostato
 
 
 
