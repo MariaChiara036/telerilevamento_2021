cb76ffefbe39663d513afbee44168ed95d06fd0b
MariaChiara036 <80686094+MariaChiara036@users.noreply.github.com>
2021-04-09 10:57:45 +0200

    Update R_code_time_series.r

--- R_code_time_series.r
+++ R_code_time_series.r
@@ -33,6 +33,38 @@ plotRGB(TGr,1,2,3, stretch="Lin")
 #se ho più valori ad esempio di blu ho valori più alti dell'immagine 2010
 plotRGB(TGr,2,3,4, stretch="Lin")
 #plot con 2005,2010,2015 nel rosso la 2005 nel verde 2010 e nel blu 2015
+library(rasterVis)
+levelplot(TGr)
+#funz levelplot dentro la libreria rasterVis
+levelplot(TGr$lst_2000)
+#plot lst_2000
+cl <- colorRampPalette(c("blue","light blue","pink","red"))(100)
+#cambio palette colori
+levelplot(TGr, col.regions=cl)
+#plot immagine con la palette prescelta
+levelplot(TGr,col.regions=cl, names.attr=c("July 2000","July 2005", "July 2010", "July 2015"))
+levelplot(TGr,col.regions=cl, main="LST variation in time", names.attr=c("July 2000","July 2005", "July 2010", "July 2015"))
+#immagine con scala di variazione sulle lst
+meltlist<-list.files(pattern="melt")
+#creo una lista coi dati melt
+import<-lapply(meltlist, raster)
+#applico funzione raster alla lista meltlist
+melt<-stack(import)
+#funzione stack a melt
+levelplot(melt)
+melt_amount<-melt$X2007annual_melt-melt$X1979annual_melt
+#differenza immagine 2007 e 1979
+clb <- colorRampPalette(c("blue","white","red"))(100)
+plot(melt_amount, col=clb)
+#plot dell'immagine che rappresenta la differenza dei ghiacci in ghiaccio sciolto tra i due anni, colore risultante è quanto ghiaccio si è sciolto tra i due estremi in anni
+levelplot(melt_amount, col=clb)
+#levelplot
+install.packages("knitr")
+
+
+
+
+
 
 
 
