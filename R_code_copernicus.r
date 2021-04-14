eb79b7ddfdba6b662430bbd1145b12e5df8cade7
MariaChiara036 <80686094+MariaChiara036@users.noreply.github.com>
2021-04-14 17:08:27 +0200

    Create R_code_copernicus.r

---
+++ R_code_copernicus.r
@@ -0,0 +1,18 @@
+#Visualizzazione copernicus
+library(raster)
+install.packages("ncdf4")
+#installato nuovo pacchetto
+setwd("C:/lab/")
+vegetation<-raster()
+vegetation<-raster("c_gls_FCOVER_199903030000_GLOBE_VGT_V1.4.1.nc")
+#inserito immagine scaricata da copernicus
+cl <- colorRampPalette(c('light blue','green','red','yellow'))(100)
+#scelti i colori
+plot(vegetation, col=cl)
+#resampling
+veget<-aggregate(vegetation, fact=100)
+#cambio la visualizzazione dell'insieme di pixel, livelli di pixel
+plot(veget, col=cl)
+
+
+ 
