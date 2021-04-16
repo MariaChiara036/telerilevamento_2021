453a5b9e7cd35b3b7ca0f6834e0553f59614d55c
MariaChiara036 <80686094+MariaChiara036@users.noreply.github.com>
2021-04-16 11:00:39 +0200

    Update R_code_knit.r

--- R_code_knit.r
+++ R_code_knit.r
@@ -1,3 +1,16 @@
 #knitr
 setwd("C:/lab/")
 library(knitr)
+stitch("R_Code_greenland.r.txt", template=system.file("misc", "knitr-template.Rnw", package="knitr"))
+#caricato codice di time series tramite report tramite funzione knitr
+
+
+
+
+
+
+
+
+
+
+
