ea9d8db8ab5c2f36c6431f837a0c42cf4a77d680
MariaChiara036 <80686094+MariaChiara036@users.noreply.github.com>
2021-04-28 12:50:28 +0200

    Update R_code_multivariate_analysis.r

--- R_code_multivariate_analysis.r
+++ R_code_multivariate_analysis.r
@@ -9,4 +9,19 @@ plot(p224)
 plot(p224$B1_sre,p224$B2_sre,col="blue",pch=19,cex=2)
 # considero solo due banda con alcuni parametri settati
 pairs(p224)
-#correlazione di vaariabili a due a due
+#correlazione di vaariabili a due a due, valori restituiti dal plot rappresentano l'indice di Pearson
+#analisi PCA impattante, ricampionare dato per rendere il lavoro più scorrevole
+#aumentare la dimensione di ogni pixel dell'immagine, diminuendo la risoluzione
+p224res<-aggregate(p224, fact=10)
+par(mfrow=c(2,1))
+plotRGB(p224, r=4,g=3,b=2, stretch="Lin")
+plotRGB(p224res, r=4,g=3,b=2, stretch="Lin")
+#confronto risoluzione tra le due immagini
+p224res_pca<-rasterPCA(p224res)
+#funzione PCA
+summary(p224res$model)
+#sommario generico p224res
+plot(p224res_pca$map)
+plotRGB(p224res_pca$map, r=1, g=2, b=3, stretch="Lin")
+#utilizzate 3 componenti principali
+
